9b32e1cdcde15032ac16953d2c8180b7eeb6ef8c
Samuel Newman <mozzius@protonmail.com>
2022-01-16 09:33:30 +0200

    finish second blog

--- components/blog-stuff.tsx
+++ components/blog-stuff.tsx
@@ -1,5 +1,6 @@
 import { useFrame, Canvas, useThree } from "@react-three/fiber";
 import React, { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import { Mesh, Vector3 } from "three";
 
 import classes from "../styles/Blog.module.scss";
@@ -42,11 +43,16 @@ export const WindowWidth = () => {
 };
 
 const DemoCanvas: React.FC = ({ children }) => {
+  const { ref, inView } = useInView();
   return (
-    <div className={classes.canvas}>
-      <Canvas dpr={typeof window !== "undefined" ? window.devicePixelRatio : 1}>
-        {children}
-      </Canvas>
+    <div ref={ref} className={classes.canvas}>
+      {inView && (
+        <Canvas
+          dpr={typeof window !== "undefined" ? window.devicePixelRatio : 1}
+        >
+          {children}
+        </Canvas>
+      )}
     </div>
   );
 };
@@ -55,13 +61,14 @@ const lerper = new Vector3();
 
 const SpinningCubeInner = () => {
   const ref = useRef<Mesh>(null!);
-  const { viewport } = useThree();
   const [hovering, setHovering] = useState(false);
 
-  useFrame(({ mouse, clock }) => {
-    ref.current.rotation.y += Math.sin(clock.getElapsedTime()) / 20;
-    ref.current.rotation.x += Math.cos(clock.getElapsedTime()) / 20;
-    ref.current.rotation.z += Math.sin(clock.getElapsedTime()) / 20;
+  useFrame(({ mouse, clock, viewport }) => {
+    ref.current.rotation.set(
+      Math.sin(clock.getElapsedTime()),
+      Math.cos(clock.getElapsedTime()),
+      Math.sin(clock.getElapsedTime())
+    );
 
     const x = (mouse.x * viewport.width) / 2;
     const y = (mouse.y * viewport.height) / 2;
@@ -78,8 +85,8 @@ const SpinningCubeInner = () => {
       onPointerEnter={() => setHovering(true)}
       onPointerLeave={() => setHovering(false)}
     >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="#ff7c94" />
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="#ff7c94" />
     </mesh>
   );
 };
@@ -88,6 +95,78 @@ export const SpinningCube = () => {
   return (
     <DemoCanvas>
       <SpinningCubeInner />
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+    </DemoCanvas>
+  );
+};
+
+export const BoringCube = () => {
+  return (
+    <DemoCanvas>
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color="#ff7c94" />
+      </mesh>
+    </DemoCanvas>
+  );
+};
+
+export const SlightlyLessBoringCube = () => {
+  return (
+    <DemoCanvas>
+      <mesh scale={2} rotation={[1, 1, 1]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="#ff7c94" />
+      </mesh>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+    </DemoCanvas>
+  );
+};
+
+// https://css-tricks.com/snippets/javascript/random-hex-color/
+const randomColour = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+export const ToggleCube = () => {
+  const [colour, setColour] = useState(randomColour);
+
+  const onChangeColour = () => setColour(randomColour);
+
+  return (
+    <DemoCanvas>
+      <mesh scale={2} rotation={[1, 1, 1]} onClick={onChangeColour}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={colour} />
+      </mesh>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+    </DemoCanvas>
+  );
+};
+
+const BasicSpinningCubeInner = () => {
+  const cubeRef = useRef<Mesh>(null!);
+
+  useFrame((_, delta) => {
+    cubeRef.current.rotation.x += delta;
+    cubeRef.current.rotation.y += delta;
+    cubeRef.current.rotation.z += delta;
+  });
+
+  return (
+    <mesh ref={cubeRef} scale={2}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="#ff7c94" />
+    </mesh>
+  );
+};
+
+export const BasicSpinningCube = () => {
+  return (
+    <DemoCanvas>
+      <BasicSpinningCubeInner />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
     </DemoCanvas>
